f0ceb53d40e26ac010ccfdefdb1062071738433b
photonena <91746642+photonena@users.noreply.github.com>
2021-12-21 18:19:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+$(function(){           //点滅
+  setInterval(function(){$('.').fadeOut(1000,function(){$(this).fadeIn(1000)});     },500);     });
+
 $('#').click(function(){                      //
     w=;
     $('#wr-section-dialoge').css('display','none');
