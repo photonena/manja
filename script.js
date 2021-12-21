d5b18d79065411cfd19abb75202e649dfdfcf249
photonena <91746642+photonena@users.noreply.github.com>
2021-12-21 18:10:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-<p>Learning Japanese with comic!</p>
+$('#').click(function(){                      //
+    w=;
+    $('#wr-section-dialoge').css('display','none');
+    $('#writing-box').css('display','block');
+    $('#wr-mini-title').text('');
+    $('#sentence1').text("");
+    $('#sentence2').text("");
+    $('#sentence3').text("");
+    $('#sentence4').text("");
+    $('#sentence5').text("");          });
