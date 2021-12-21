d9b685ab4d30c39dfb2f6b58154f061d446a65a7
photonena <91746642+photonena@users.noreply.github.com>
2021-12-21 18:28:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 $(function(){           //点滅
-  setInterval(function(){$('.').fadeOut(1000,function(){$(this).fadeIn(1000)});     },500);     });
+  setInterval(function(){$('.img1').fadeOut(1000,function(){$(this).fadeIn(1000)});     },500);     });
 
 $('#').click(function(){                      //
     w=;
     $('#wr-section-dialoge').css('display','none');
     $('#writing-box').css('display','block');
-    $('#wr-mini-title').text('');
-    $('#sentence1').text("");
-    $('#sentence2').text("");
-    $('#sentence3').text("");
-    $('#sentence4').text("");
-    $('#sentence5').text("");          });
+    $('#wr-mini-title').text('');          });
+$(function(){           //フェードアウト
+  $('#img1').click(function(){setInterval(function(){$('.img1').fadeOut(1000,function(){$(this).fadeIn(1000)});     },500);     });   });
