868cd0b630584d495dd833544a2534f9564876a0
photonena <91746642+photonena@users.noreply.github.com>
2021-12-23 23:46:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-$(function(){           //点滅
-  setInterval(function(){$('.img1').fadeOut(1000,function(){$(this).fadeIn(1000)});     },500);     });
-
-$('#').click(function(){                      //
-    w=;
-    $('#wr-section-dialoge').css('display','none');
-    $('#writing-box').css('display','block');
-    $('#wr-mini-title').text('');          });
-$(function(){           //フェードアウト
-  $('#img1').click(function(){setInterval(function(){$('.img1').fadeOut(1000,function(){$(this).fadeIn(1000)});     },500);     });   });
+$(function(){           //クリックで転換
+    $('#img1').click(function(){setTimeout(function(){$('#img1').fadeOut(1000,function(){$('#img2').fadeIn(1000)});     },500);     });
+    $('#img2').click(function(){setTimeout(function(){$('#img2').fadeOut(1000,function(){$('#img3').fadeIn(1000)});     },500);     });
+    $('#img3').click(function(){setTimeout(function(){$('#img3').fadeOut(1000,function(){$('#img4').fadeIn(1000)});     },500);     });
+    $('#img4').click(function(){setTimeout(function(){$('#img4').fadeOut(1000,function(){$('#img1').fadeIn(1000)});     },500);     });   });
+$(function(){           //クリックで転換
+    $('#tag1').click(function(){setTimeout(function(){$('#contact').fadeOut(1000,function(){$('#about').fadeIn(1000)});     },500);     });
+    $('#tag1').click(function(){setTimeout(function(){$('#imgs').fadeOut(1000,function(){$('#about').fadeIn(1000)});     },500);     });
+    $('#tag2').click(function(){setTimeout(function(){$('#about').fadeOut(1000,function(){$('#imgs').fadeIn(1000)});     },500);     });
+    $('#tag2').click(function(){setTimeout(function(){$('#contact').fadeOut(1000,function(){$('#imgs').fadeIn(1000)});     },500);     });
+    $('#tag3').click(function(){setTimeout(function(){$('#imgs').fadeOut(1000,function(){$('#contact').fadeIn(1000)});     },500);     });
+    $('#tag3').click(function(){setTimeout(function(){$('#about').fadeOut(1000,function(){$('#contact').fadeIn(1000)});     },500);     });   });
